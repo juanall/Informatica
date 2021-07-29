c0a482c09cfeb657af414c70b136fc450465b6a7
juanall <juanallaneza@hotmail.com>
2021-07-29 11:08:55 -0300

    TP 6
    Objetos

---
+++ TP6.py/6.1.py
@@ -0,0 +1,55 @@
+
+#OBJETOS:
+# #Ejercicio 1
+#Dadas las siguientes clases responder:
+
+#cuales son sus estados
+
+#cuales son sus interfaces
+
+#¿Comparten interfaz?
+
+#¿Son polimórficas?
+
+class Perro:
+    def __init__(self):
+        self.alimento = 0
+        self.caricias = 0
+
+    def energia(self):
+        return self.alimento + (self.caricias * 10)
+
+    def comer(self, gramos):
+        self.alimento += gramos
+
+    def alimento(self):
+	    print(self.alimento)
+
+    def acariciar(self):
+        self.caricias += 1
+
+    def estaDebil(self):
+        return self._caricias < 2
+
+    def pasear(self, km):
+	    self.alimento -= km / 4
+
+class Gato:
+    def __init__(self):
+        self.alimento = 0
+        self.caricias = 0
+
+    def energia(self):
+        return self.alimento + (self.caricias * 8)
+
+    def comer(self, gramos):
+        self.alimento += gramos * 1.5
+
+    def caricias(self):
+	    print(self.caricias)
+
+    def acariciar(self):
+        self.caricias += 1
+
+    def estaDebil(self):
+        return self._caricias < 4
---
+++ TP6.py/6.2.py
@@ -0,0 +1,3 @@
+#Ejercicio 2
+#Modificar la clase Golondrina vista en la teoría para poder preguntar si una golondrina está en equilibrio. Este estado se alcanza cuando la energía se encuentra entre 150 y 300.
+
